5d306b963c2703238e5a7a215227c98a3211aeb7
Biswajeet Sahoo <bmsahoorourkela@gmail.com>
2026-02-21 18:19:42 +0530

    js fundamentals

---
+++ Biswajeet-Sahoo/Javascript/index.js
@@ -0,0 +1,11 @@
+console.log(a)
+var a=10 
+{
+    let name="biswajeet";
+    console.log(name)
+    {
+        console.log(name);
+        //let name="Biswajeet"
+        console.log(name)
+    }
+g
