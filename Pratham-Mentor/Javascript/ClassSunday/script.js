5dbff14788196702b415dabb888f83121e72829b
Pratham Srivastava <pratham.shriv@gmail.com>
2026-02-22 13:08:21 +0530

    Class 1 notes

---
+++ Pratham-Mentor/Javascript/ClassSunday/script.js
@@ -0,0 +1,68 @@
+// console.log('hello')
+// console.log(20)
+// console.log(true)
+
+// int x = 10;
+// String s = "hello"
+
+// var x = 10
+// let y = "hello"
+// const z = "world";
+
+// Var
+// var is globally and functional scopedd
+// redeclare and reinitialize
+
+
+//let
+//let globally and block scope
+//reinitialize but cannot redeclare
+
+// var x = 10
+
+
+// var x; // declaration
+// x = 10; // iniitialization
+// x = 11; //re-assignement
+
+// var x = 10;
+// let x = 10
+// x = 12;
+
+
+// inside local scope
+// {
+//     // let x = 41;
+// }
+
+// function name() {
+//     var y = 0;
+//     console.log(x)
+//     console.log(y)
+// }
+
+// // console.log(y)
+
+// name()
+
+
+/*
+const
+global and blocked scope
+redeclare and reinitialize both not possible
+value cannot be changed
+*/
+
+// const x = 10;
+// x =11; not possible
+
+// {
+//     let x = 10
+//     console.log(x)
+//     {
+//         // local var x and global var x
+//         console.log(x)
+//         let x = 11
+//     }
+//     console.log(x)
+// }
