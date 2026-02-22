4d074f07790cc756c64c279157b4674e12fbf1e3
Max Rex <trex.eternatus@gmail.com>
2026-02-22 15:53:37 +0530

    Javascript second class - Updated

--- Arko-Roy-Chowdhury/JavaScript/script.js
+++ Arko-Roy-Chowdhury/JavaScript/script.js
@@ -143,3 +143,108 @@
 // //async function returns a promise
 
 // asy()
+
+
+// async function asy(){
+//     console.log("Start")
+
+//     const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//     const val = function(){}
+//     const val2 = () => {}
+
+//     //setTimeout(fn, time in ms)
+
+//     console.log(data)
+
+//     console.log("End")
+// }
+
+// async function asy(){
+//     console.log("Start")
+
+//     const data = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then(() => { //resolved
+//         console.log("Success")
+//     })
+//     .catch(() => { //rejected
+//         console.log("Error")
+//     })
+//     .finally(() => { //always executes
+//         console.log("Process Completed")
+//     });
+
+//     //setTimeout(fn, time in ms)
+
+//     console.log(data)
+
+//     console.log("End")
+// }
+
+//asy()
+
+// async function asy(){
+//     console.log("Start")
+
+//     const data = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((...args) => { //resolved
+//         console.log("Success")
+//         console.log(args)
+//     })
+//     .catch(() => { //rejected
+//         console.log("Error")
+//     })
+//     .finally(() => { //always executes
+//         console.log("Process Completed")
+//     })
+//     ;
+
+
+
+//     console.log("End")
+// }
+
+//asy()
+
+// async function asy(){
+//     console.log("Start")
+
+//     const data = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((val) => { //resolved
+//         console.log("Success")
+//         console.log(val)
+//     })
+//     .catch((err) => { //rejected
+//         console.log("Error")
+//         console.log(err)
+//     })
+//     .finally(() => { //always executes
+//         console.log("Process Completed")
+//     })
+//     ;
+
+
+
+//     console.log("End")
+// }
+
+// function name(){
+//     console.log(arguments)
+// }
+
+// name(1,"Hello",3)
+
+// const arrow = () => {
+//     console.log(arguments)
+// }
+
+// arrow(1,"Hello",3) // ReferenceError: arguments is not defined
+
+// const arr = [1, 2, 3, 4, 5]
+
+// console.log(...arr) //Spread Operator - allows an iterable such as an array or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.
+
+// const arrow = (...val) => {
+//     console.log(val)
+// }
+
+// arrow(1,"Hello",3) 
