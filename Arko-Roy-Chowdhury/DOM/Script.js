8f554d2bc42f4d37ff642ca73c803d66fcd34282
Max Rex <trex.eternatus@gmail.com>
2026-03-07 17:32:08 +0530

    First DOM Class

--- Arko-Roy-Chowdhury/DOM/Script.js
+++ Arko-Roy-Chowdhury/DOM/Script.js
@@ -7,7 +7,24 @@
 // ele.style.fontSize = "40px";
 // ele.style.textAlign = "center";
 
-let element = document.getElementsByClassName("Second");
-console.log(element);
-console.log(element[0]);
-console.log(typeof element);
+// let element = document.getElementsByClassName("Second");
+// console.log(element);
+// console.log(element[0]);
+// console.log(typeof element);
+
+// let elements = document.getElementsByClassName("Second");
+// for (let i = 0; i < elements.length; i++)
+// {
+//     console.log(elements[i].innerText);
+// }
+
+// for(ele of elements)
+// {
+//     console.log(ele.innerText);
+// }
+
+let tag = document.getElementsByTagName("h1");
+for (let i = 0; i < tag.length; i++)
+{
+    console.log(tag[i].innerText);
+}
