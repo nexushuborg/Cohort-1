b15bd670e6cfa7b3da75711685e7e90627b718dc
G Smruti Shriya <gsmrutishriya12@gmail.com>
2026-03-07 17:59:14 +0530

    07/03/2026

---
+++ G-Smruti-Shriya/JavaScript/07-03-2006(DOM Manipulation)/script.js
@@ -0,0 +1,44 @@
+// let ele = document.getElementById("First-title")
+// console.log(ele)
+// ele.innerText = "Hello"
+
+// let elem = document.getElementsByClassName("1class")
+// console.log(elem)
+// elem.innerText = "cant say much"
+// console.log(elem[0])
+// console.log(typeof elem)
+
+// let e = document.getElementsByClassName("2class")
+// e.innerText = "cant say much"
+// for(let i = 0 ; i < e.length ; i++){
+//     console.log(e[i].innerText);
+// }
+
+// for (i of elem){
+//     console.log(ele.innerText)
+// }
+
+// let tag = document.getElementsByTagName("h1")
+// for(let i = 0;i<tag.length ; i++){
+//     console.log(tag[i].innerText)
+// }
+// console.log(tag)
+
+// let q =  document.querySelector("h1")
+// console.log(q)
+// for(let i = 0;i<q.length;i++){
+//     console.log(q[i].innerText)
+// }
+
+// let q1 =  document.querySelectorAll("h1")
+// console.log(q1)
+// for(let i = 0;i<q1.length;i++){
+//     console.log(q1[i].innerText)
+// }
+// for(ele of q1){
+//     console.log(ele.innerText)
+// }
+
+const el = document.querySelector("div")
+console.log(el.textContent)
+console,log(el.innerText)
