642551db27616aa5b373cfd36b8bb77d97b9bd28
Abhirup Kovid <abhirupkovid@gmail.com>
2026-03-10 18:30:43 +0530

    contact form js

--- Abhirup-Kovid/JavaScript/DOM/DOM.js
+++ Abhirup-Kovid/JavaScript/DOM/DOM.js
@@ -1,4 +1,27 @@
+
+//DOM-Document Object Model
+//JS does not directly manipulates the html
+//it uses DOM representation of HTML
+//DOM representation is the tree like structure thatd created when the webpage loads a HTML page
+//before attaching events js must find elements Two common methods for that is :
+//1: getElementById(): Finds the element of html by its id
+//2: querySelector(): works like css selector document.querySelector("button")
+
+// what is Event?
+// any action that happens in the webpage is called event
+
+//Event handling?
+//running the js code when event occurs 
+//example <button id="btn">click</button>
+
+//two ways to handle an event 
+//1. inline:js code is directly written inside html
+//2. addEventListener():js separately written
+
+
+
 // <!-- how the browser works
+
 
 // 1. you write html code 
 // 2. browser fetches the html file
@@ -57,13 +80,13 @@
 // let ele = document.querySelector(".m-2")
 // console.log(ele)
 
-let cont=document.getElementById('div content')
-cont.style.backgroundColor='yellow'
-cont.style.margin='20px'
-cont.style.padding='20px'
-cont.style.height='90px'
-cont.style.border='10px solid black'
-cont.style.display='flex'
-cont.style.justifyContent='center'
-cont.style.fontSize='24px'
-cont.style.borderRadius='50%'
+// let cont=document.getElementById('div content')
+// cont.style.backgroundColor='yellow'
+// cont.style.margin='20px'
+// cont.style.padding='20px'
+// cont.style.height='90px'
+// cont.style.border='10px solid black'
+// cont.style.display='flex'
+// cont.style.justifyContent='center'
+// cont.style.fontSize='24px'
+// cont.style.borderRadius='50%'
