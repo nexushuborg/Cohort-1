89e155d0633fcb49ded4dda977cdea8fcb485399
priyanshu7250 <priyanshujha7250@gmail.com>
2026-03-11 21:15:33 +0530

    11march2026

--- Priyanshu-Kumar-Jha/DOM/script.js
+++ Priyanshu-Kumar-Jha/DOM/script.js
@@ -38,3 +38,44 @@
 const element = document.querySelector("div");
 console.log(element.textContent)// sara text content la ke de dega.
 console.log(element.innerText)// jo available text hai wahi la ke dega.
+
+
+//lecture 11 march 2026;
+
+let div = document.getElementById("div-container")
+    const btn = documment.createElement("button")
+    btn.textContent="click me"
+    div.prepend(btn)
+    div.removeChild(btn);
+
+
+
+
+    //Event handling
+    // const btn = document.getElementById("Btn")
+//on click with button
+    // btn.onclick = function(){
+    //     console.log("1st")
+    // }
+
+//addEventListner with all the elements;
+// btn.addElementListner("mouseenter",function()){
+//     this.style.background
+// }
+// <form if = "rsgestrationform">
+//     <div>
+//         <label for = "username">Usrename:</label>
+//         <input type = "text" id = "email"name="email"
+//         placeholder = "Enter email"></input>
+//     </div>
+//     <br>
+//     <button tyoe = "submit">Register</button>
+//     </br>
+// </form>
+const form = document.getElementById("registrationform")
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+})
+
+//value reading
+const username = document.getElementById("usrname")
