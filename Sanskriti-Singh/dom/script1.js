482f85920c47c8447b18c3ad84aeda7b1e97d56c
Sanskriti Singh <sanskritisingh3287@gmail.com>
2026-03-11 21:16:02 +0530

    form validation

--- Sanskriti-Singh/dom/script1.js
+++ Sanskriti-Singh/dom/script1.js
@@ -8,7 +8,7 @@
 // console.log(element);
 // console.log(element[0])
 
-let element=document.getElementsByClassName("main")
+// let element=document.getElementsByClassName("main")
 // for(let i=0;i<element.length;i++){
 //     console.log(element[i].innerText)
 // }
@@ -24,7 +24,7 @@ let element=document.getElementsByClassName("main")
 // }
 
 //querySelector - it does sabka kaam
-let q=document.querySelectorAll(".main")//without all it access the first
+// let q=document.querySelectorAll(".main")//without all it access the first
 // console.log(q)
 
 //Dom properties
@@ -33,12 +33,54 @@ let q=document.querySelectorAll(".main")//without all it access the first
 // console.log(q.innerText)
 // q.innerText=str
 // console.log(q.innerText)
-let cont=document.getElementById('div-container')
-cont.style.background='yellow'
-cont.style.fontsize='24px'
-cont.style.padding='20px'
-cont.style.margin='50px'
-cont.style.border='12px solid blue'
-cont.style.display="flex"
-cont.style.justifyContent="centre"
-cont.style.alignment="centre"
+// let cont=document.getElementById('div-container')
+// cont.style.background='yellow'
+// cont.style.fontsize='24px'
+// cont.style.padding='20px'
+// cont.style.margin='50px'
+// cont.style.border='12px solid blue'
+// cont.style.display="flex"
+// cont.style.justifyContent="centre"
+// cont.style.alignment="centre"
+
+
+//setting button
+// let div= document.getElementById("div-container")
+
+// const btn= document.createElement("button")
+// btn.textContent="click me"
+// div.append(btn)
+
+//event handling
+// const btn=document.getElementById("btn")
+// btn.onclick=function(){
+//  alert("hello world")
+// }
+// btn.addEventListener("mouseover",function(){
+//     this.style.backgroundColor="red"
+// })
+// btn.addEventListener("mouseover",function(){
+//     this.style.backgroundColor="blue"
+// })
+
+const form=document.getElementById("registrationform")
+
+form.addEventListener("submit",function(ele){
+    ele.preventDefault()//page reload prevented
+    //value reading
+    const username=document.getElementById("username").value.trim
+const email =document.getElementById("email").value
+
+console.log("username", username)
+console.log("email",email)
+//form validation: checking the values of username and email to validate int the form
+if(username===""||email==""){
+    alert("please fill in all the field")
+}
+//"this refers to the current present element =might be a div or an input field or any form element "
+if(username.length<3){
+    alert("usrname must be 3 character long")
+    this.style.border="2px solid red"
+}
+})
+
