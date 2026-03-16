38fe0273a4b3a8cb0743c07675339d0384a6085b
shreyasinha328-sys <shreyasinha328@gmail.com>
2026-03-16 21:48:51 +0530

    Added HTML file and updated script

--- Shreya-Sinha/DOM/script.js
+++ Shreya-Sinha/DOM/script.js
@@ -1,3 +1,12 @@
+//const div=document.getElementById("div-container")
+//function redColor(){
+// const element=document.querySelector("#three")//this for getting the id three
+// element.className = "red" //here we are changing the properties to the red class
+//}
+
+//but.onclick = redColor
+
+//but.addEventListener('click',redColor))//here it takes two argument
 let div=document.getElementById("div-container")
 
 const btn = document.createElement("button")
