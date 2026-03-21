29c741389320e704514108acdb0e71a5bff687c3
your name <subhankardasmohanty@gmail.com>
2026-03-21 18:18:29 +0530

    project 1

---
+++ Subhankar-Das-Mohanty/project/project1/style.js
@@ -0,0 +1,18 @@
+const form=document.getElementById("signupForm");
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  let username = document.getElementById("username").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    if(username === ""){
+      
+        
+    }
+    if(username.length < 5){
+        
+    }
+    if(username)
+
+
+}
