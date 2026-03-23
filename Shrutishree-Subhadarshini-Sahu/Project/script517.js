98b2edb126fa0990c4b40f964143fb6d3b6658e9
shrutishree subhadarshini sahu <shrutishreessahu@gmail.com>
2026-03-23 19:02:14 +0530

    project

---
+++ Shrutishree-Subhadarshini-Sahu/Project/script517.js
@@ -0,0 +1,6 @@
+function login(){
+    let email=document.getElementById("email").value
+    let password=document.getElementById("password").value
+}
+let storedEmail = localStorage.getItem("email");
+let storedPassword = localStorage.getItem("password");
