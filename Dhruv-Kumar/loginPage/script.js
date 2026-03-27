88f953b57f9f70acb980ecbaea1d3bfcbad3b47f
Dhruv <dhruvkumar24280@gmail.com>
2026-03-27 17:55:43 +0530

    Drag and Drop Feature

--- Dhruv-Kumar/loginPage/script.js
+++ Dhruv-Kumar/loginPage/script.js
@@ -8,16 +8,28 @@ logIn.addEventListener('submit', (e) => {
     const passwords = document.getElementById('password').value;
     const conpwds = document.getElementById('confirmPwd').value
 
-    //now checking the emails 
-    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    let isValid = emails.match(regex);
+    //now checking the emails and passwords
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{6,}$/;
     
-    if(!isValid){
+    let isEmailValid = emailRegex.test(emails);
+    let isPasswordValid = passwordRegex.test(passwords);
+    
+    if(!isEmailValid){
         alert("Invalid Email address");
-        
+        e.preventDefault();
+        return;
     }
+
+    if(!isPasswordValid){
+        alert("Invalid password: Must be at least 6 characters and contain both letters and numbers.");
+        e.preventDefault();
+        return;
+    }
+
     if(passwords!=conpwds){
         alert("passwords not valid");
+        e.preventDefault();
         return;
     }
 
