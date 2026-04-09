6c0ff76856b092ce9291aabfc7af4aa129e07e02
Max-Rex-2006 <blacklynx2006@gmail.com>
2026-04-09 23:23:19 +0530

    Changed version code-oss to vscode

---
+++ Arko-Roy-Chowdhury/React/Component.jsx
@@ -0,0 +1,21 @@
+import component from "./component";
+import "./style.css";
+
+export default function App() {
+    return (
+        <>
+            <Component />
+            <Component />
+            
+        </> //Fragment
+
+    );
+}
+
+
+//JSX, Props, States
+
+//RoadsideCoder
+//Akshay Saini
+//Piyush Garg
+//Chai aur Code
